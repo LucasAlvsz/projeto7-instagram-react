bf2f4a6a75d83984d61459a344c5e510618a0bb0
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-03-14 03:10:50 -0300

    Ajustes finais

--- src/Corpo/Esquerda/Posts/Post/Post.js
+++ src/Corpo/Esquerda/Posts/Post/Post.js
@@ -4,9 +4,18 @@ import Fundo from "./Fundo/Fundo"
 export default function Post({ imgUserUrl, nomeUsuario, srcConteudo, curtidasImgUser, curtidasNomeUsuario, curtidasNum }) {
     return (
         <div class="post">
-            <Topo imgUserUrl={imgUserUrl} nomeUsuario={nomeUsuario} />
-            <Conteudo srcConteudo={srcConteudo} />
-            <Fundo curtidasImgUser={curtidasImgUser} curtidasNomeUsuario={curtidasNomeUsuario} curtidasNum={curtidasNum} />
+            <Topo
+                imgUserUrl={imgUserUrl}
+                nomeUsuario={nomeUsuario}
+            />
+            <Conteudo
+                srcConteudo={srcConteudo}
+            />
+            <Fundo
+                curtidasImgUser={curtidasImgUser}
+                curtidasNomeUsuario={curtidasNomeUsuario}
+                curtidasNum={curtidasNum}
+            />
         </div>
     )
 }
